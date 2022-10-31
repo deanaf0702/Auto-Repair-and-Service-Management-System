80bd9856486eafe0686cc741566a0dd3eea59600
tsenck12 <tsenck@ncsu.edu>
2022-10-31 01:31:16 -0400

    Setting up basic application flow

--- src/frontend/AUTORApplication.java
+++ src/frontend/AUTORApplication.java
@@ -1,10 +1,45 @@
 package frontend;
 
+import java.util.Scanner;
 
 public class AUTORApplication {
-	public static void main(String[] args) {
-		System.out.println("This is the start of the application");
-	}
-	//Commented
 	
+	enum Role {
+	    ADMIN,
+	    MANAGER,
+	    RECEPTIONIST,
+	    CUSTOMER,
+	    MECHANIC
+	  }
+	
+	/** keep track of current user */
+	public int userId;
+	public Role userRole;
+	
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Welcome to AUTOR.\n 1. Login \n 2. Sign Up \n 3. System Admin \n 4. Exit \nChoose an Option (1,2,3): ");
+		while (!in.hasNextInt()) {
+			in.nextLine();
+			System.out.print("Welcome to AUTOR.\n 1. Login \n 2. Sign Up \n 3. System Admin \n 4. Exit \nChoose an Option (1,2,3): ");
+		}
+		int choice = in.nextInt();
+		in.close();
+		switch (choice) {
+			case 1:
+			// Login code
+				break;
+			case 2:
+				// Sign Up code
+				break;
+			case 3:
+				// Admin code
+				break;
+			case 4:
+				System.exit(0);
+				break;
+			default:
+		}
+	}
 }
