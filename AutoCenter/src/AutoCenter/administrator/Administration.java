a82ba947507e3ce3b8c817f87e54d0229763f1e3
pulanski <joshuakersey50@gmail.com>
2022-11-03 22:49:09 -0400

    refactor(ui): refactor displays for more rich ui
    refactor displays for formatting in consistent manner

--- AutoCenter/src/AutoCenter/administrator/Administration.java
+++ AutoCenter/src/AutoCenter/administrator/Administration.java
@@ -2,59 +2,60 @@ package AutoCenter.administrator;
 
 import AutoCenter.Interface;
 import AutoCenter.ScanHelper;
-import AutoCenter.manager.AddNewEmployee;
-import AutoCenter.manager.SetupStore;
 import AutoCenter.services.UserService;
 
-public class Administration implements Interface{
+public class Administration implements Interface {
 
-	private UserService userservice = null;
-	
-	public Administration()
-	{
-		userservice = new UserService();
-	}
-	@Override
-	public void run() {
-		int selection = 4;
-		display();
-		do{
-			System.out.println("Enter choice(1-4)");
-			selection = ScanHelper.nextInt();
-		}while(!(selection >=1 && selection <=4));
-		
-		navigate(selection);
-	}
+    private UserService userservice = null;
 
-	@Override
-	public void display() {
-		System.out.println("##### Administration Page #####");
-		System.out.println("1 System Set Up");
-		System.out.println("2 Add New Store");
-		System.out.println("3 Add New Service");
-		System.out.println("4 Logout");
-		System.out.println("##########");
-	}
+    public Administration() {
+        userservice = new UserService();
+    }
 
-	@Override
-	public void navigate(int selection) {
-		switch (selection)
-		{
-			case 1: new SystemSetUp().run();
-				break;
-			case 2: new AddNewStore().run();
-				break;
-			case 3: new AddNewService().run();
-			break;
-			case 4: goBack();
-				break;
-		}
-	}
+    @Override
+    public void run() {
+        int selection = 4;
+        display();
+        do {
+            System.out.print("Enter choice (1-4) from the given options displayed above: ");
+            selection = ScanHelper.nextInt();
+        } while (!(selection >= 1 && selection <= 4));
 
-	@Override
-	public void goBack() {
-		userservice.logout();
-		
-	}
+        navigate(selection);
+    }
 
+    @Override
+    public void display() {
+        System.out.println("##############################");
+        System.out.println("##### Admin Landing Page #####");
+        System.out.println("##############################");
+        System.out.println("# 1 System Set Up            #");
+        System.out.println("# 2 Add New Store            #");
+        System.out.println("# 3 Add New Service          #");
+        System.out.println("# 4 Logout                   #");
+        System.out.println("##############################");
+    }
+
+    @Override
+    public void navigate(int selection) {
+        switch (selection) {
+            case 1:
+                new SystemSetUp().run();
+                break;
+            case 2:
+                new AddNewStore().run();
+                break;
+            case 3:
+                new AddNewService().run();
+                break;
+            case 4:
+                goBack();
+                break;
+        }
+    }
+
+    @Override
+    public void goBack() {
+        userservice.logout();
+    }
 }
--- AutoCenter/src/AutoCenter/customer/ScheduleService.java
+++ AutoCenter/src/AutoCenter/customer/ScheduleService.java
@@ -2,53 +2,52 @@ package AutoCenter.customer;
 
 import AutoCenter.Interface;
 import AutoCenter.ScanHelper;
-import AutoCenter.manager.AddNewEmployee;
-import AutoCenter.manager.SetupStore;
 
-public class ScheduleService implements Interface{
+public class ScheduleService implements Interface {
 
-	@Override
-	public void run() {
-		int selection = 0;
-		display();
-		do{
-			System.out.println("Enter choice(1-4)");
-			selection = ScanHelper.nextInt();
-		}while(!(selection >=1 && selection <=4));
-		
-		navigate(selection);
-		
-	}
+    @Override
+    public void run() {
+        int selection = 0;
+        display();
+        do {
+            System.out.println("Enter choice(1-4)");
+            selection = ScanHelper.nextInt();
+        } while (!(selection >= 1 && selection <= 4));
 
-	@Override
-	public void display() {
-		System.out.println("##### Schedule Service Menu #####");
-		System.out.println("1 Add Schedule Maintenance");
-		System.out.println("2 Add Schedule Repair");
-		System.out.println("3 View cart and select schedule time");
-		System.out.println("4 Go Back");
-		System.out.println("##########");
-	}
+        navigate(selection);
+    }
 
-	@Override
-	public void navigate(int selection) {
-		switch (selection)
-		{
-			case 1: new AddScheduleMaintenance().run();
-				break;
-			case 2: new AddScheduleRepair().run();
-				break;
-			case 3: new ViewCartandSelectScheduleTime().run();
-				break;
-			case 5: goBack();
-		}
-		
-	}
+    @Override
+    public void display() {
+        System.out.println("##########################################");
+        System.out.println("##### Customer Schedule Service Menu #####");
+        System.out.println("##########################################");
+        System.out.println("# 1 Add Schedule Maintenance             #");
+        System.out.println("# 2 Add Schedule Repair                  #");
+        System.out.println("# 3 View cart and select schedule time   #");
+        System.out.println("# 4 Go Back                              #");
+        System.out.println("##########################################");
+    }
 
-	@Override
-	public void goBack() {
-		new ScheduleService().run();
-		
-	}
+    @Override
+    public void navigate(int selection) {
+        switch (selection) {
+            case 1:
+                new AddScheduleMaintenance().run();
+                break;
+            case 2:
+                new AddScheduleRepair().run();
+                break;
+            case 3:
+                new ViewCartandSelectScheduleTime().run();
+                break;
+            case 5:
+                goBack();
+        }
+    }
 
+    @Override
+    public void goBack() {
+        new ScheduleService().run();
+    }
 }
