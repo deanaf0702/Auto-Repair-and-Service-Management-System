5d0393294433936c1568d549407c97c5b59fdf3f
pulanski <joshuakersey50@gmail.com>
2022-11-05 16:22:10 -0400

    feat(admin): add admin system setup
    add admin system setup logic and ui

--- AutoCenter/src/AutoCenter/administrator/Administration.java
+++ AutoCenter/src/AutoCenter/administrator/Administration.java
@@ -6,56 +6,64 @@ import AutoCenter.services.UserService;
 
 public class Administration implements Interface {
 
-  private UserService userService = null;
+    private static final int MIN_SELECTION = 1;
+    private static final int MAX_SELECTION = 4;
 
-  public Administration() {
-    userService = new UserService();
-  }
+    private static final String MENU_SEPARATOR = "###############################";
 
-  @Override
-  public void run() {
-    int selection = 4;
-    display();
-    do {
-      System.out.print("Enter choice (1-4) from the given options displayed above: ");
-      selection = ScanHelper.nextInt();
-    } while (!(selection >= 1 && selection <= 4));
+    private UserService userService = null;
 
-    navigate(selection);
-  }
-
-  @Override
-  public void display() {
-    System.out.println("###############################");
-    System.out.println("##### Admin: Landing Page #####");
-    System.out.println("###############################");
-    System.out.println("# 1 System Set Up             #");
-    System.out.println("# 2 Add New Store             #");
-    System.out.println("# 3 Add New Service           #");
-    System.out.println("# 4 Logout                    #");
-    System.out.println("###############################");
-  }
-
-  @Override
-  public void navigate(int selection) {
-    switch (selection) {
-      case 1:
-        new SystemSetUp().run();
-        break;
-      case 2:
-        new AddNewStore().run();
-        break;
-      case 3:
-        new AddNewService().run();
-        break;
-      case 4:
-        goBack();
-        break;
+    public Administration() {
+        userService = new UserService();
     }
-  }
 
-  @Override
-  public void goBack() {
-    userService.logout();
-  }
+    @Override
+    public void run() {
+        int selection;
+        display();
+        do {
+            System.out.print("Enter choice (1-4) from the given options displayed above: ");
+            selection = ScanHelper.nextInt();
+        } while (!(selection >= MIN_SELECTION && selection <= MAX_SELECTION));
+
+        navigate(selection);
+    }
+
+    @Override
+    public void display() {
+        System.out.println(MENU_SEPARATOR);
+        System.out.println("##### Admin: Landing Page #####");
+        System.out.println(MENU_SEPARATOR);
+        System.out.println("# 1 System Set Up             #");
+        System.out.println("# 2 Add New Store             #");
+        System.out.println("# 3 Add New Service           #");
+        System.out.println("# 4 Logout                    #");
+        System.out.println(MENU_SEPARATOR);
+    }
+
+    @Override
+    public void navigate(int selection) {
+        switch (selection) {
+            case 1:
+                new SystemSetUp().run();
+                break;
+            case 2:
+                new AddNewStore().run();
+                break;
+            case 3:
+                new AddNewService().run();
+                break;
+            case 4:
+                goBack();
+                break;
+            default:
+                System.out.println("Invalid selection.");
+                break;
+        }
+    }
+
+    @Override
+    public void goBack() {
+        userService.logout();
+    }
 }
