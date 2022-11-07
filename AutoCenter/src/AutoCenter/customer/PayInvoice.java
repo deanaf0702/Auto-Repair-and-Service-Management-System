a3b98489fd0f02c8ee4c883933a7e804de4c28e5
tsenck12 <tsenck@ncsu.edu>
2022-11-07 02:29:36 -0500

    Remaining implementation

--- AutoCenter/src/AutoCenter/customer/PayInvoice.java
+++ AutoCenter/src/AutoCenter/customer/PayInvoice.java
@@ -1,30 +1,61 @@
 package AutoCenter.customer;
 
+import AutoCenter.ScanHelper;
 import AutoCenter.UserFlowFunctionality;
+import AutoCenter.repository.DbConnection;
 
 public class PayInvoice implements UserFlowFunctionality {
 
     @Override
-    public void run() {
+    public void run () {
+        System.out.println( "Enter ID of invoice to pay: " );
+        final int id = ScanHelper.nextInt();
+        final String query = "update ServiceEvents set isPaid = 1 where serviceEventId = " + id;
+        display();
+        int selection = 0;
+        do {
+            System.out.print( "Enter choice (1-3) from the given options displayed above: " );
+            selection = ScanHelper.nextInt();
+        }
+        while ( ! ( selection >= 1 && selection <= 2 ) );
+
+        if ( selection == 2 ) {
+            goBack();
+        }
+        final DbConnection db = new DbConnection();
+        try {
+            db.executeUpdate( query );
+            System.out.println( "Invoice paid successfully" );
+        }
+        catch ( final Exception e ) {
+            System.out.println( "Error retrieving invoices" );
+            goBack();
+        }
+        finally {
+            db.close();
+        }
+        new PayInvoice().run();
+    }
+
+    @Override
+    public void display () {
+        System.out.println( "####################################################" );
+        System.out.println( "##### Customer: Pay Invoices Menu              #####" );
+        System.out.println( "####################################################" );
+        System.out.println( "# 1 Pay Invoice                                    #" );
+        System.out.println( "# 2 Go Back                                        #" );
+        System.out.println( "####################################################" );
+    }
+
+    @Override
+    public void navigate ( final int selection ) {
         // TODO Auto-generated method stub
 
     }
 
     @Override
-    public void display() {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void navigate(int selection) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void goBack() {
-        // TODO Auto-generated method stub
+    public void goBack () {
+        new Invoices().run();
 
     }
 }
--- AutoCenter/src/AutoCenter/customer/ViewInvoiceDetails.java
+++ AutoCenter/src/AutoCenter/customer/ViewInvoiceDetails.java
@@ -1,30 +1,53 @@
 package AutoCenter.customer;
 
+import java.util.List;
+
+import AutoCenter.ScanHelper;
 import AutoCenter.UserFlowFunctionality;
 
 public class ViewInvoiceDetails implements UserFlowFunctionality {
 
     @Override
-    public void run() {
+    public void run () {
+        System.out.print( "Enter id of invoice to view: " );
+        final int id = ScanHelper.nextInt();
+        final List<Integer> invoiceList = Invoices.getInvoiceIds();
+        if ( !invoiceList.contains( id ) ) {
+            new Invoices().run();
+        }
+        display();
+        int selection = 0;
+        do {
+            System.out.print( "Enter choice (1-2) from the given options displayed above: " );
+            selection = ScanHelper.nextInt();
+        }
+        while ( ! ( selection >= 1 && selection <= 2 ) );
+        if ( selection == 2 ) {
+            goBack();
+        }
+        final String query = "select from ServiceEvents se inner join CustomerVehicles cv on se.vin = cv.vin"
+                + "inner join Users u on u.userId = se.mechanicId";
+
+    }
+
+    @Override
+    public void display () {
+        System.out.println( "###################################" );
+        System.out.println( "##### Customer: View Invoices #####" );
+        System.out.println( "###################################" );
+        System.out.println( "# 1 View Invoice                  #" );
+        System.out.println( "# 2 Go Back                       #" );
+        System.out.println( "###################################" );
+    }
+
+    @Override
+    public void navigate ( final int selection ) {
         // TODO Auto-generated method stub
 
     }
 
     @Override
-    public void display() {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void navigate(int selection) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void goBack() {
-        // TODO Auto-generated method stub
-
+    public void goBack () {
+        new Invoices().run();
     }
 }
